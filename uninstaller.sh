08c18871c15057eaa95ea1617fa1e6aebf686a79
victor <gictorbit@gmail.com>
2020-02-14 09:03:44 +0330

    ask question function added

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-main (){
+main(){
     check_arg $1
-    echo "shah"
+    ask_question "you are uninstalling photoshop cc v19 are you sure?" "N"
 }
 
-error (){
+error(){
     echo -e "\033[1;31merror:\e[0m $@"
     exit 1
 }
@@ -12,6 +12,26 @@ error (){
 check_arg(){
     if [ $1 != 0 ];then
         error "please just run script without any argument"
+    fi
+}
+
+#parameters [Message] [default flag [Y/N]]
+function ask_question(){
+    result=""
+    if [ "$2" == "Y" ];then
+        read -r -p "$1 [Y/n] " response
+        if [[ "$response" =~ $(locale noexpr) ]];then
+            result="no"
+        else
+            result="yes"
+        fi
+    elif [ "$2" == "N" ];then
+        read -r -p "$1 [N/y] " response
+        if [[ "$response" =~ $(locale yesexpr) ]];then
+            result="yes"
+        else
+            result="no"
+        fi
     fi
 }
 
