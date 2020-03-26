a614d3eddafe45e3f808730d825ecc4defd2a113
victor <gictorbit@gmail.com>
2020-03-26 17:36:34 +0430

    add winecfg script

---
+++ winecfg.sh
@@ -0,0 +1,14 @@
+SCR_PATH="$HOME/.photoshopCCV19"
+CACHE_PATH="$HOME/.cache/photoshopCCV19" 
+WINE_PATH="$SCR_PATH/wine-3.4" 
+RESOURCES_PATH="$SCR_PATH/resources"
+WINE_PREFIX="$SCR_PATH/prefix" 
+export WINEPREFIX="$WINE_PREFIX"
+export PATH="$WINE_PATH/bin:$PATH"
+export LD_LIBRARY_PATH="$WINE_PATH/lib:$LD_LIBRARY_PATH"
+#export WINEDLLOVERRIDES="winemenubuilder.exe=d"
+export WINESERVER="$WINE_PATH/bin/wineserver"
+export WINELOADER="$WINE_PATH/bin/wine"
+export WINEDLLPATH="$WINE_PATH/lib/wine"
+
+winecfg
