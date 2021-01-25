1dc0d180e45d79b17459d5d13901284a3da2a05d
Maksymilian Potok <makspotok@gmail.com>
2021-01-25 17:50:34 +0100

    fixed package managers

--- scripts/sharedFuncs.sh
+++ scripts/sharedFuncs.sh
@@ -14,11 +14,24 @@ function package_installed() {
         if [ "$pkginstalled" -eq 0 ];then
             show_message "package\033[1;36m $1\e[0m is installed..."
         else
-            warning "package\033[1;33m $1\e[0m is not installed.\nplease make sure it's already installed"
-            ask_question "would you continue?" "N"
+            warning "package\033[1;33m $1\e[0m is not installed."
+            ask_question "would you like to install it now?" "N"
             if [ "$question_result" == "no" ];then
                 echo "exit..."
                 exit 5
+            else
+                declare -A osInfo;
+                osInfo[/etc/redhat-release]=yum upgrade && yum install
+                osInfo[/etc/arch-release]=pacman -Syu && pacman -S
+                osInfo[/etc/SuSE-release]=zypper update && zypper install
+                osInfo[/etc/debian_version]=apt-get update && apt-get install
+
+                for f in ${!osInfo[@]}
+                do
+                    if [[ -f $f ]];then
+                        sudo ${osInfo[$f]} $1
+                    fi
+                done
             fi
         fi
     fi
